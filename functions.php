d7189dc32235a0570cb0ad94e82e60663dc3153b
duckdivers <howard@duckdiverllc.com>
2020-12-20 14:31:27 -0500

    Init Repo

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+//require get_stylesheet_directory() . '/customizer/customizer-framework.php';
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = '_s-style'; // This is the name of the stylesheet for DuckDiverFramework
+ 
+    //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
+    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri(). '/bootstrap/bootstrap.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_style( 'theme-woocommerce', get_stylesheet_directory_uri(). '/css/woocommerce.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/custom.css',
+        array( $parent_style ),
+        filemtime(get_stylesheet_directory() . '/custom.css')
+    );
+}
+
+// Uncomment to remove CPT from Parent Theme
+/*function remove_parent_theme_cpts(){
+    remove_action('init', 'my_post_type_header_image');
+}
+add_action('after_setup_theme', 'remove_parent_theme_cpts'); */
---
+++ inc/child-theme-options.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Customizer Controls.
+ *
+ * @package WPshed Customizer Framework
+ */
+
+// User access level
+$capability = 'edit_theme_options';
+
+// Option type
+//$type = 'theme_mod'; // option / theme_mod
+//
+//$options[] = array( 'title'             => __( 'Theme Options', 'text-domain' ),
+//                    'description'       => '',
+//                    'id'                => 'child_theme_options',
+//                    'priority'          => 10,
+//                    'theme_supports'    => '',
+//                    'type'              => 'panel' );
+//// Section
+//$options[] = array( 'title'             => __( 'Reserved', 'text-domain' ),
+//                    'description'       => __( 'Section description', 'text-domain' ),
+//                    'panel'             => 'child_theme_options',
+//                    'id'                => 'slug_section_id',
+//                    'priority'          => 10,
+//                    'theme_supports'    => '',
+//                    'type'              => 'section' );
+//
+//
+// OPTIONS FOR HOMEPAGE YOUTUBE VIDEO
+/* $options[] = array( 'title'             => __( 'Homepage Video', 'dd_theme' ),
+                    'description'       => __( 'Use the youtube video ID only', 'dd_theme' ),
+                    'section'           => 'dd_theme_options',
+                    'id'                => 'homepage-video',
+					'default'			=> '1',
+                    'option'            => 'text',
+                    'sanitize_callback' => 'sanitize_text_field',
+                    'type'              => 'control' );
+*/					
