bb0212b0603267347f2a795ff07a746a85b48d97
Howard <howard@duckdiverllc.com>
2020-12-21 09:02:50 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function my_theme_enqueue_styles() {
  
     //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
     wp_enqueue_style( 'theme-woocommerce', get_stylesheet_directory_uri(). '/css/woocommerce.min.css', array(), '4.3.1', 'all');
-    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri(). '/bootstrap/bootstrap.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_style( 'theme-bootstrap', get_stylesheet_directory_uri(). '/bootstrap/bootstrap.min.css', array(), '4.3.1', 'all');
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/custom.css',
         array( $parent_style ),
